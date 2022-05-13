58649c7e328f8e38b4033316e6db47420b30b7dc
taveira1904 <74012001+taveira1904@users.noreply.github.com>
2022-05-13 14:16:30 +0100

    Add files via upload

---
+++ paginas.js
@@ -0,0 +1,12 @@
+ //Javacript for the scroll indicator bar
+ window.addEventListener("scroll", () => {
+    const indicatorBar = document.querySelector(".scroll-indicator-bar");
+
+    const pageScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollValue = (pageScroll / height) * 100;
+
+    indicatorBar.style.width = scrollValue + "%";
+  });
+  
+
